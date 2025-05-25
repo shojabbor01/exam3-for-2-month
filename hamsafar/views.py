b91334abf96ec3afbe729f91a90196964525ad43
H4CKER <aqdodovirshod7@gmail.com>
2025-05-25 16:52:10 +0500

    Added views part 2

--- hamsafar/views.py
+++ hamsafar/views.py
@@ -24,6 +24,34 @@ def register(request):
     return render(request, 'register.html')
 
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    
+    return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            return redirect('login')
+
+    return render(request, 'register.html')
+
+def logout(request):
+    logout(request)
+    return redirect('login')
 
 
 
